1c65882098f1fe5e046af0b360e2cfc1802c00ef
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2023-01-24 21:50:46 +0300

    0-safe_print_list update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
         except:
+            print("except")
             break
         else:
             count += 1
