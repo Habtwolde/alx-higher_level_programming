8d1d16e5f422d01a4f0e62a43ae2e8c9f70195eb
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2023-01-25 00:10:47 +0300

    0000

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-no module imported
-"""
+"""Defines a class Square"""
 
 
 class Square:
-    """
-    Empty class that defines a square
+    """Class that defines properties of square.
     """
     pass
