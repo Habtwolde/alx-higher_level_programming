fa0aa4dcb2a2b88e2f84b726aac5d829c139d9ad
Habtamu Wolde <39864976+Habtwolde@users.noreply.github.com>
2023-01-30 18:57:43 +0300

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
+"""Rectangle Class.
+This module contains an empty class that defines a rectangle.
+Usage Example:
+    Rectangle = __import__('0-rectangle').Rectangle
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
+"""
+
+
 class Rectangle:
-    """ Empty class """
+    """Defines the blueprint of a rectangle"""
+
     pass
